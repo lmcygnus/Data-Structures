2ba21855473ad5a99c86c4a20f440d6fb077e581
lmcygnus <mloida591@gmail.com>
2024-04-24 14:09:34 -0400

    add merge sort funcion

--- BST.js
+++ BST.js
@@ -1,7 +1,17 @@
+/* eslint-disable max-classes-per-file */
 class Node {
-    constructor(data) {
-      this.data = data;
-      this.left = null;
-      this.right = null;
-    }
+  constructor(data) {
+    this.data = data;
+    this.left = null;
+    this.right = null;
   }
+}
+class Tree {
+  constructor(arr) {
+    this.root = this.buildTree(arr);
+  }
+
+  buildTree(arr) {
+
+  }
+}
---
+++ merge.js
@@ -0,0 +1,25 @@
+function mergeSort(arr) {
+  if (arr.length <= 1) {
+    return arr;
+  }
+
+  const mid = Math.floor(arr.length / 2);
+  const left = mergeSort(arr.slice(0, mid));
+  const right = mergeSort(arr.slice(mid));
+
+  const result = [];
+  let leftIndex = 0;
+  let rightIndex = 0;
+
+  while (leftIndex < left.length && rightIndex < right.length) {
+    if (left[leftIndex] < right[rightIndex]) {
+      result.push(left[leftIndex]);
+      leftIndex += 1;
+    } else {
+      result.push(right[rightIndex]);
+      rightIndex += 1;
+    }
+  }
+
+  return result.concat(left.slice(leftIndex)).concat(right.slice(rightIndex));
+}
