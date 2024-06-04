114ff5539cd8c972e8e5e993894623ac3a5c01a2
lmcygnus <mloida591@gmail.com>
2024-06-04 10:41:15 -0400

    function to check if the moves contains the end move

--- Ktravails.js
+++ Ktravails.js
@@ -1,12 +1,11 @@
-const board = [[1, 2, 3, 4, 5, 6, 7, 8],
-  [0, 1, 2, 3, 4, 5, 6, 7],
-  [0, 1, 2, 3, 4, 5, 6, 7],
-  [0, 1, 2, 3, 4, 5, 6, 7],
-  [0, 1, 2, 3, 4, 5, 6, 7],
-  [0, 1, 2, 3, 4, 5, 6, 7],
-  [0, 1, 2, 3, 4, 5, 6, 7],
-  [0, 1, 2, 3, 4, 5, 6, 7],
-];
+/* eslint-disable no-restricted-syntax */
+class Position {
+  constructor(coord) {
+    this.cood = coord;
+    this.distance = null;
+    this.predecesor = null;
+  }
+}
 
 function allMoves(x, y) {
   const moves = [[x + 1, y - 2],
@@ -21,7 +20,14 @@ function allMoves(x, y) {
   for (const move of moves) {
     array.push(move.filter((value) => value >= 0 && value < 8));
   }
-  return array.filter((element) => element.length == 2);
+  array.filter((element) => element.length === 2);
 }
 
 console.log(allMoves(7, 7));
+
+function containsEnd(array, end) {
+  if (array.find((move) => move[0] === end[0] && move[1] === end[1])) {
+    return true;
+  }
+}
+
